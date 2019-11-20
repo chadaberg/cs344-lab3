81d88cfb77b423c5a073c14bec70555b915b56d0
Chad Berg <bergcha@oregonstate.edu>
2019-11-20 15:34:49 -0800

    Initial shell push

---
+++ smallsh.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+
+void shell_loop();
+
+char* read_line();
+
+char** get_args();
+
+
+
+
+
+
+int main(){
+
+
+    return 0;
+}
