c5e9c049b2db40c5f955d29cee2987ac68eddd40
Chad Berg <bergcha@oregonstate.edu>
2019-11-22 18:17:35 -0800

    Added Input and some parsing capabilities

--- smallsh.c
+++ smallsh.c
@@ -8,32 +8,40 @@
 
 void shell_loop();
 
-char* read_line();
+char* read_line(char* line);
 
-char** get_args();
+char** get_args(char* line, int lineSize);
+
+int get_lineSize(char* line);
 
 
 int main(){
     do{
-        printf("\n:");
-        fflush();
-        char* line = read_line();
-        char** args = get_args();
-    }while(1);
+        int lineSize;
+        printf(":");
+        fflush(stdin);
+        char line[MAXCHAR];
+        read_line(line);
+        lineSize = get_lineSize(line);
 
+        char** args = get_args(line, lineSize);
+
+        for(int i = 0; i < lineSize; i++){
+            printf("%c", line[i]);
+        }
+        printf("\n");
+    }while(1);
     return 0;
 }
 
 //Read Line
 //This function reads input from stdin and moves it into a string
 //This function returns a string containing the entire line of input
-char* read_line(){
-    char* line = malloc(sizeof(char) * 2048)
-    
-    char* buffer;
-    buffer = readline();
-    
-    strcpy(line, buffer);
+char* read_line(char * line){
+
+    size_t LINESIZE = MAXCHAR;
+
+    getline(&line, &LINESIZE,stdin);   
 
     return line;
 }
@@ -41,7 +49,25 @@ char* read_line(){
 //Get Arguments
 //This function uses the string from read line to seperate the line into individual arguements.
 //This function returns an array of strings containing the arguments
-char** get_args(char* line){
-    char* args[] = malloc(sizeof(char*) * 512);
+char** get_args(char* line, int lineSize){
+    char** args = malloc(sizeof(char*) * 512);
+    int numSpaces = 0;
+    for(int i = 0; i < lineSize; i++){
+        if(line[i] == ' '){
+            numSpaces++;
+        }
+    }
+    printf("%d", numSpaces);
+    int numArgs;
     return args;
+}
+
+int get_lineSize(char* line){
+    int size;
+    for(size = 0; size < MAXCHAR; size++){
+        if(line[size] == '\n'){
+            break;
+        }
+    }
+    return size;
 }
